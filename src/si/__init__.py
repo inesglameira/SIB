add6e67a8e7fcc66772eeb54799dfba3db4fbfe2
inesglameira <pg40082@uminho.pt>
2025-09-22 11:56:27 +0100

    change authorship

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Capela"
+__author__ = "Inês Lameira"
 __credits__ = ["João Capela", "João Correia, Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Capela"
-__email__ = "joao.capela@ceb.uminho.pt"
+__maintainer__ = "Inês Lameira"
+__email__ = "inesguedeslameira@gmail.com"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
