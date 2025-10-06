9d1c20fa180274b7c850038aa60bba4e9f4bf590
inesglameira <pg40082@uminho.pt>
2025-10-06 11:46:42 +0100

    add

---
+++ src/si/feature_selection/variance_threshold.py
@@ -0,0 +1,20 @@
+from si.base.transformer import Transformer
+from si.data.dataset import Dataset
+
+class VarianceThreshold(Transformer):
+
+    def __init__(self, threshold: float, **kwargs):
+        self.threshold = threshold
+        self.variances = None
+    
+    def _fit(self, dataset: Dataset) -> 'VarianceThreshold':
+        self.variances = np.var(dataset.X, axis=0)
+
+    def _transform(self, dataset: Dataset) -> Dataset:
+        mask = self.variance >= self.threshold
+        X = dataset.X[: , mask]
+        features = np.array(dataset.features)[mask]
+        return Dataset(X=X, features = features, y = dataset.y, label = dataset.label)
+
+
+        
