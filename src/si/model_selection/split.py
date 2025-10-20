2c4355a82497caf8340024ca03ef9f49071c36ee
inesglameira <pg40082@uminho.pt>
2025-10-20 11:16:23 +0100

    changes

--- src/si/model_selection/split.py
+++ src/si/model_selection/split.py
@@ -6,4 +6,13 @@ def train_test_split(dataset: Dataset, test_size: float, random_state: int = 42)
     n_samples = dataset.shape()[0]
     test_samples = int(n_samples * test_size)
     permutations = np.random.permutation(n_samples)
+    test_indexes = permutations[:test_samples]
+    train_indexes = permutations[test_samples:]
+
+    train_dataset = Dataset( X = dataset.X[train_indexes], y = dataset.y[train_indexes], 
+                            features = dataset.features, label = dataset.label)
     
+    test_dataset = Dataset( X = dataset.X[train_indexes], y = dataset.y[train_indexes], 
+                            features = dataset.features, label = dataset.label)
+    
+    return train_dataset, test_dataset
